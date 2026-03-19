3dfd2dc76374497f5d7576e8a92020eed30da66e
Yasin Hakim <ibnkhaqim@gmail.com>
2026-03-19 17:54:50 +0700

    Fitur cari definisi, sinonim, dan definisi dari kata sinonim.

--- app.py
+++ app.py
@@ -1,7 +1,41 @@
-import flask
 import requests
+import flask
+from flask import Flask, render_template, request, redirect, url_for
 
-def cari_kata(kata: str) -> list:
+app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    hasil_definisi = []
+    hasil_kata_relevan = []
+    kata_dicari = None
+    
+    if request.method == "POST":
+        kata_dicari = request.form.get('keyword')
+        return redirect(url_for('index', q=kata_dicari))
+    elif request.method == "GET":
+        query = request.args.get("q")
+        if query != None:
+            kata_dicari = query
+            hasil_definisi, hasil_kata_relevan = cari_kata(query)
+        else:
+            hasil_definisi = []
+            hasil_kata_relevan = []
+            kata_dicari = None
+    
+    return render_template('index.html',
+                           definitions = hasil_definisi,
+                           relations = hasil_kata_relevan,
+                           keyword = kata_dicari,
+                           )
+
+@app.route("/api/definisi/<string:kata>")
+def definisi_kata(kata):
+    definisi = jabar_kata(kata)
+    return flask.jsonify(definisi)
+
+def cari_kata(kata: str) -> tuple[list, list]:
     try:
         url = 'https://kateglo.lostfocus.org/api.php?format=json&phrase='+kata
         r = requests.get(url, timeout=5)
@@ -12,17 +46,37 @@ def cari_kata(kata: str) -> list:
         data = None
         return
     definitions = []
+    
     for i in range(len(data['kateglo']['definition'])):
-        definitions.append(data['kateglo']['definition'][i]['def_text'])
+        try:
+            definitions.append(data['kateglo']['definition'][i]['def_text'])
+        except requests.exceptions.RequestException as e:
+            print(f"Terjadi Kesalahan: {e}")
+            data = None
     
-    relations = []
-    for i in range(len(data['kateglo']['all_relation'])):
-        relations.append(data['kateglo']['all_relation'][i]['related_phrase'])
-    
+    relations = {}
+    for i in range(len(data['kateglo']['relation']['s'])-1):
+        relations[data['kateglo']['relation']['s'][str(i)]['related_phrase']] = data['kateglo']['relation']['s'][str(i)]['rel_type_name']
     return definitions, relations
 
-def jabar_kata(kata):
-    # TODO: Menjabarkan kata yang berelasi
-    return kata
+def jabar_kata(kata: str) -> list:
+    try:
+        url = 'https://kateglo.lostfocus.org/api.php?format=json&phrase='+kata
+        r = requests.get(url)
+        r.raise_for_status()
+        data = r.json()
+    except requests.exceptions.RequestException as e:
+        print(f"Terjadi Kesalahan: {e}")
+        data = None
+    definitions = []
+    for i in range(len(data['kateglo']['definition'])):
+        try:
+            definitions.append(data['kateglo']['definition'][i]['def_text'])
+        except requests.exceptions.RequestException as e:
+            print(f"Terjadi Kesalahan: {e}")
+            data = None
+    return definitions
 
-definitions, relations = cari_kata("kata")
+
+if __name__ == "__main__":
+    app.run()
